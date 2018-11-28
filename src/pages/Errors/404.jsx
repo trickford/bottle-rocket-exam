3682329aad2be8bc24d9af22697ba2410e43eb37
Rick Ford <rickfordrick@gmail.com>
2018-11-28 16:13:51 -0600

    add 404 page

---
+++ src/pages/Errors/404.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+class Error404 extends React.PureComponent {
+    render() {
+        return (
+            <Fragment>
+                <div>404 - Page not found.</div>
+            </Fragment>
+        );
+    }
+}
+
+export default Error404;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AppFrame from '../components/AppFrame/AppFrame';
 import Home from '../pages/Home/Home';
+import Error404 from '../pages/Errors/404';
 
 const AppRouter = (
 	<Router>
 		<AppFrame>
 			<Route exact path="/" component={Home} />
+			<Route component={Error404} />
 		</AppFrame>
 	</Router>
 );
